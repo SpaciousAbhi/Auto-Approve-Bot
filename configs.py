468d0d8673d70f26ef65a67cee09984830f39fdf
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-04-04 17:55:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", "22663326"))
     API_HASH = getenv("API_HASH", "927e822ca6e854d8b7369c72ab9506e4")
     BOT_TOKEN = getenv("BOT_TOKEN", "6756845327:AAGwyRCE2YlVZQihOhJzbWj2qz7J7rx4dU8")
-    FSUB = getenv("FSUB", "your channel username")
-    CHID = int(getenv("CHID", "your channel id"))
+    FSUB = getenv("FSUB", "PremiumCheapDealsOffers")
+    CHID = int(getenv("CHID", "-1002025332256"))
     SUDO = list(map(int, getenv("1654334233").split()))
     MONGO_URI = getenv("mongodb+srv://tigamow711:s0cUEhrE1Jb5Iehs@vsfsb.pb89igt.mongodb.net/", "mongodb url")
     
