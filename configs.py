4b51f655382d30093efc9580ea188fe60131f4e1
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-04-04 18:02:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "6756845327:AAGwyRCE2YlVZQihOhJzbWj2qz7J7rx4dU8")
     FSUB = getenv("FSUB", "PremiumCheapDealsOffers")
     CHID = int(getenv("CHID", "-1002025332256"))
-    SUDO = list(map(int, getenv("1654334233").split()))
+    SUDO = list(map(int, getenv("1654334233", "").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://tigamow711:s0cUEhrE1Jb5Iehs@vsfsb.pb89igt.mongodb.net/")
     
 cfg = Config()
